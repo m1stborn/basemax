2c34963f928deecb3ff835f860e9b64e0cf47626
m1stborn <r10922178@ntu.edu.tw>
2022-04-28 05:10:22 +0800

    Feat: integrate basic redis database

--- data_controller.py
+++ data_controller.py
@@ -1,15 +1,29 @@
+import os
 import json
 from pathlib import Path
 
+import redis
+
+ON_HEROKU = os.environ.get('ON_HEROKU', None)
+
+if ON_HEROKU:
+    REDIS_URL = os.environ.get('REDIS_URL ', None)
+else:
+    config = json.loads(Path('config.json').read_text())
+    REDIS_URL = config["REDIS_URL"]
+
+r = redis.from_url(REDIS_URL)
+
 
 def get_game_data():
-    game_infos = json.loads(Path("games.json").read_text(encoding="utf-8"))
-
+    # game_infos = json.loads(Path("games.json").read_text(encoding="utf-8"))
+    game_infos = json.loads(r.get('games').decode('utf-8'))
     return game_infos
 
 
 def get_game_title():
-    game_infos = json.loads(Path("games.json").read_text(encoding="utf-8"))
+    # game_infos = json.loads(Path("games.json").read_text(encoding="utf-8"))
+    game_infos = json.loads(r.get('games').decode('utf-8'))
     game_titles = {url: f"{game['team_away']}vs{game['team_home']}".strip()
                    for url, game in game_infos.items()}
 
@@ -17,16 +31,18 @@ def get_game_title():
 
 
 def update_broadcast_list(game_url, user_id):
-    users_to_broadcast = json.loads(Path("broadcast_list.json").read_text(encoding="utf-8"))
+    # broadcast_list = json.loads(Path("broadcast_list.json").read_text(encoding="utf-8"))
+    broadcast_list = json.loads(r.get('broadcast_list').decode('utf-8'))
 
-    if user_id not in users_to_broadcast[game_url]:
-        users_to_broadcast[game_url].append(user_id)
+    if user_id not in broadcast_list[game_url]:
+        broadcast_list[game_url].append(user_id)
 
     with open('broadcast_list.json', 'w', encoding="utf-8") as f:
-        json.dump(users_to_broadcast, f, indent=4, ensure_ascii=False)
+        json.dump(broadcast_list, f, indent=4, ensure_ascii=False)
 
 
 def get_broadcast_list(game_url):
-    users_to_broadcast = json.loads(Path("broadcast_list.json").read_text(encoding="utf-8"))
+    broadcast_list = json.loads(r.get('broadcast_list').decode('utf-8'))
+    # users_to_broadcast = json.loads(Path("broadcast_list.json").read_text(encoding="utf-8"))
 
-    return users_to_broadcast[game_url]
+    return broadcast_list[game_url]
