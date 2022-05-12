65a8b23d2e55981ab912ee31889530f452cb4b03
m1stborn <r10922178@ntu.edu.tw>
2022-05-12 18:51:51 +0800

    Feat: add standing schemas

--- schemas/standing.py
+++ schemas/standing.py
@@ -14,7 +14,7 @@ class Team(BaseModel):
     @classmethod
     def from_list(cls, row: List[str]):
         # Hard coding the scraped list
-        values = [row[0][0], row[0][1:]] + row[2].split("-") + row[3:5]
+        values = [row[0][0], row[0][1:]] + row[2].split("-") + [row[3].replace("0", "")] + [row[4]]
         keys = cls.__fields__.keys()
         team_dict = dict(zip(keys, values))
 
