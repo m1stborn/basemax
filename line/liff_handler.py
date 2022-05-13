f6d86e18cb9483a5534f7a89b0f16b5214d1b98b
m1stborn <r10922178@ntu.edu.tw>
2022-05-14 04:36:35 +0800

    Feat: add liff redirect template

--- line/liff_handler.py
+++ line/liff_handler.py
@@ -2,7 +2,7 @@ import json
 import os
 from pathlib import Path
 
-from flask import Blueprint, Response, render_template
+from flask import request, Blueprint, Response, render_template
 
 from line.game_flex import flex_message_wrapper
 from line.standing_flex import (
@@ -19,22 +19,34 @@ else:
 liff_blueprint = Blueprint('liff', __name__, template_folder="./templates")
 
 
-@liff_blueprint.route("/liff/share/liff.state", methods=['get'])
-def liff_state_handler():
-    return Response(render_template('liff_redirect.html', liff_id=LIFF_ID))
-
-
-@liff_blueprint.route("/liff/share/standing", methods=['get'])
-def liff_handler():
+@liff_blueprint.route("/liff/share", methods=['GET'])
+def liff_page():
     alt = "分享CPBL戰績排行"
     contents = standing_content(footer=False)
     flex = flex_message_wrapper(alt, contents)
 
-    # elif stream:
-    #     alt = "CPBL球隊戰績"
-    #     content.append(stream_flex_template(row.id, row.title, row.image, row.link))
-    # else:
-    #     content = [add_me()]
-    # return msg, LIFF_ID
-    return Response(render_template('share_message.html', flex=flex, liff_id=LIFF_ID))
+    if request.args.get("life.state"):
+        return Response(render_template('liff_redirect.html', liff_id=LIFF_ID))
+    else:
+        return Response(render_template('share_message.html', flex=flex, liff_id=LIFF_ID))
+
+
+# @liff_blueprint.route("/liff/share/liff.state", methods=['GET'])
+# def liff_state_handler():
+#     return Response(render_template('liff_redirect.html', liff_id=LIFF_ID))
+#
+#
+# @liff_blueprint.route("/liff/share/standing", methods=['GET'])
+# def liff_handler():
+#     alt = "分享CPBL戰績排行"
+#     contents = standing_content(footer=False)
+#     flex = flex_message_wrapper(alt, contents)
+#
+#     # elif stream:
+#     #     alt = "CPBL球隊戰績"
+#     #     content.append(stream_flex_template(row.id, row.title, row.image, row.link))
+#     # else:
+#     #     content = [add_me()]
+#     # return msg, LIFF_ID
+#     return Response(render_template('share_message.html', flex=flex, liff_id=LIFF_ID))
 
