2a049e8445446452003b7baf28374206c05cceb6
m1stborn <r10922178@ntu.edu.tw>
2022-06-25 21:10:52 +0800

    Feat: line notify endpoint

---
+++ models/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from config import Setting
+
+config = Setting()
+
+engine = create_engine(config.DATABASE_URL)
+Base = declarative_base()
+
+
+def create_session():
+    session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    session = session_local()
+
+    return session
